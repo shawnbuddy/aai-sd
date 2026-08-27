3a717cbbfab159cc69cdb161f1461eecf42a46b6
Shawn Hopkins <shawnhopkins@Administrators-MacBook-Pro.local>
2026-08-27 15:51:25 -0400

    Add migrated content templates

--- wp-content/themes/sd_base/single.php
+++ wp-content/themes/sd_base/single.php
@@ -1,12 +1,17 @@
 <?php
+
 /**
- * Default single template.
+ * @file
+ * Routes singular content to its Timber template.
  */
 
 use Timber\Timber;
 
 $context = Timber::context();
 $context['post'] = Timber::get_post();
-$context['template'] = 'single';
+$post_type = get_post_type();
+$kentico_post_types = array_keys(sd_kentico_content_model());
+$kentico_post_types[] = 'tribe_events';
+$context['template'] = in_array($post_type, $kentico_post_types, TRUE) ? 'single-' . $post_type : 'single';
 
 Timber::render(['src/4-pages/base.twig'], $context);
